2f279678778422e6c77fc63fbe599f91d054434f
banker <sospeterkinyanjui57@gmail.com>
2026-06-15 06:17:49 +0300

    more cleanup for main.go and migrations files for users

--- backend/pkg/db/migration/sqlite/000001_create_users_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id         INTEGER PRIMARY KEY AUTOINCREMENT,
-    email      TEXT NOT NULL UNIQUE,
-    password   TEXT NOT NULL,
-    first_name TEXT NOT NULL,
-    last_name  TEXT NOT NULL,
-    is_private INTEGER DEFAULT 0,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
